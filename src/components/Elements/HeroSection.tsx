2f9e323fe1ad252232c4de6549f79abd00caec5d
Randy Steven <randysteven12@gmail.com>
2024-12-07 15:18:06 +0700

    add localisation and shadow

--- src/components/Elements/HeroSection.tsx
+++ src/components/Elements/HeroSection.tsx
@@ -1,17 +1,27 @@
+'use client';
+import { useTitleContentHook } from "@/hooks/titleContentHook"
 import { Fragment } from "react"
 
 export const HeroSection = () => {
+    let title = useTitleContentHook()
     return <>
-        <Fragment>
-            <div 
-                className={`
-                    h-96 bg-cover bg-center 
-                    bg-[url(/images/hero_section.jpg)] 
-                    flex items-center 
-                    justify-center`
-                }>
-                <h1 className="text-black font-bold text-4xl align-middle">Welcome to Next.js</h1>
-            </div>    
-        </Fragment>
+            <Fragment>
+                <div 
+                    className={`
+                        h-96 bg-cover bg-center 
+                        flex items-center 
+                        justify-center`
+                    }
+                    style={{
+                        backgroundImage: 
+                            `url('/images/hero_section.jpg')`
+                    }}>
+                    <h1 className="text-white text-4xl font-bold align-middle">
+                        <span className="drop-shadow-xl [text-shadow:_0_3px_3px_rgb(0_0_0_/_50%)]">
+                            {title}
+                        </span>
+                    </h1>
+                </div>    
+            </Fragment>
     </>
 }
---
+++ src/hooks/titleContentHook.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react"
+import { useLocalization } from "./localizationHook"
+import { TitleContent } from "@/interfaces/contents/TitleContent"
+
+export const useTitleContentHook = () : string => {
+    const localization = useLocalization()
+
+    const [titleContent, setTitleContent] = useState<string>("")
+
+    useEffect(()=> {
+        fetch(`/contents/json/${localization}/title.json`).
+        then((response)=> {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json()
+        }).
+        then((data) => setTitleContent(data.title))
+    }, [localization])
+
+    return titleContent
+}
